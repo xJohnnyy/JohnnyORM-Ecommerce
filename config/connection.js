0451d30348d3a26d91332a17c6c58d352467dd0a
xJohnnyy <152817770+xJohnnyy@users.noreply.github.com>
2024-04-09 23:00:49 -0600

    debug

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
+
 const Sequelize = require('sequelize');
+
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
@@ -9,4 +11,5 @@ const sequelize = process.env.JAWSDB_URL
         decimalNumbers: true,
       },
     });
+
 module.exports = sequelize;
