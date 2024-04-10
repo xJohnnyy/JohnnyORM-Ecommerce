4605d566dc9cd911379599fe9b8a3bd24254393c
xJohnnyy <152817770+xJohnnyy@users.noreply.github.com>
2024-04-09 22:15:56 -0600

    debug

--- Develop/seeds/index.js
+++ Develop/seeds/index.js
@@ -2,7 +2,9 @@ const seedCategories = require('./category-seeds');
 const seedProducts = require('./product-seeds');
 const seedTags = require('./tag-seeds');
 const seedProductTags = require('./product-tag-seeds');
+
 const sequelize = require('../config/connection');
+
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
